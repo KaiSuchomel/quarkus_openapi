65c6756876dc87caa2af45300be316edc1cfd96c
Kai Suchomel <kai.suchomel@gec.io>
2023-11-15 14:15:54 +0100

    removed unneeded classes, only produce archunit warnings in JDK21 build

---
+++ src/main/java/org/acme/domain/MyMain.java
@@ -0,0 +1,14 @@
+package org.acme.domain;
+
+public class MyMain {
+    
+        
+    /**
+     * Enable the following Code Snippet to fail ArchunitRules
+     */
+    private void antiPattern() {
+        throw new RuntimeException("This is an antipattern!!!");
+    }
+  
+    
+}
